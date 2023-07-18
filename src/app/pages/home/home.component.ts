011363e43e04db200ed096c7236508b5b63052cb
Godstime Osarobo <nosarobo@gmail.com>
2023-07-18 06:24:30 +0100

    added facebook pixel gtag ad fixes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,12 +2,15 @@ import { Component, OnInit } from "@angular/core";
 import { ArcService } from "src/app/services/arc.service";
 import { CircleLogoImage } from "src/app/components/icons";
 
+declare var fbq: Function;
+declare var gtag: Function;
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"]
 })
+
 export class HomeComponent implements OnInit {
   defaultImage = `https://www.atmosair.com/wp-content/themes/atmosair/assets/icons/loading-spinner-white-thin.gif`;
   CircleLogoImage = CircleLogoImage;
@@ -21,6 +24,17 @@ export class HomeComponent implements OnInit {
         inline: "nearest"
       });
     }
+  }
+
+  tagEvent(action: string, category: string, label: string) {
+    gtag("event", action, {
+      event_category: category,
+      event_label: label
+    });
+    fbq("track", action, {
+      content_category: category,
+      content_name: label
+    });
   }
 
   ngOnInit(): void {}
